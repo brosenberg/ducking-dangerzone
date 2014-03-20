31f176ac111d628b478b1724a0d8ca764481c466
brosenberg <brosenberg@pistoncloud.com>
2014-03-19 23:06:40 -0700

    Dependencies in full attack and roll full attack

--- roller/roller.py
+++ roller/roller.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
+import argparse
+import random
 import json
+
+def roll(die, sides):
+    r = 0
+    for i in range(die):
+        r += random.randint(1,sides)
+    return r
+
 
 class Attack(object):
     def __init__(self, data):
@@ -17,12 +26,30 @@ class Attack(object):
                     self.damage_types[damage_type]['modifier'] = 0
 
     def __str__(self):
+        attack = ''
+        # I think the data structure for this might need to be rethought.
+        try:
+            for hit_type, hit in self.data['hit'].items():
+                attack += "+%s %s " % (hit, hit_type.upper())
+        except KeyError:
+            pass
         damage = []
         for dt in self.damage_types:
-            mod = "+%s" % (self.damage_types[dt]['modifier'],) if self.damage_types[dt]['modifier'] > 0 else ''
+            mod = "+%s" % (self.damage_types[dt]['modifier'],) if self.damage_types[dt]['modifier'] != 0 else ''
             s = "%sd%s%s %s" %(self.damage_types[dt]['dice'], self.damage_types[dt]['sides'], mod, dt)
             damage.append(s)
-        return ' +'.join(damage)
+        return attack + ' +'.join(damage)
+
+    def roll(self):
+        attack = {'hit':{}, 'damage':{}}
+        try:
+            for hit_type, hit in self.data['hit'].items():
+                attack['hit'][hit_type] = roll(1,20)+hit
+        except KeyError:
+            pass
+        for dt in self.damage_types:
+            attack['damage'][dt] = roll(self.damage_types[dt]['dice'], self.damage_types[dt]['sides']) + self.damage_types[dt]['modifier']
+        return attack
 
 
 class Character(object):
@@ -33,14 +60,21 @@ class Character(object):
             self.attacks[attack] = Attack(self.data['attacks'][attack])
 
     def fullattack(self):
-        for attack in self.data['full_attack']:
-            print "%s: %s" % (attack.title(), self.attacks[attack])
+        for attack in sorted(self.data['full_attack']):
+            try:
+                print "if %s hit" % (' and '.join(self.data['full_attack'][attack]['depends']),)
+            except KeyError:
+                pass
+            print "%8s: %s" % (attack, self.attacks[ self.data['full_attack'][attack]['attack'] ])
+            print self.attacks[ self.data['full_attack'][attack]['attack'] ].roll()
 
 
 def main():
-    # TODO: Use argparse to specify this
-    json_file = 'vorogoth.json'
-    data = json.load(open(json_file))
+    p = argparse.ArgumentParser(description="Destroy your enemies!")
+    p.add_argument('-j', '--json', type=str, required=True, help='JSON file to load stats from')
+    args = p.parse_args()
+
+    data = json.load(open(args.json))
     c = Character(data)
     c.fullattack()
 
