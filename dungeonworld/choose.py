90380d89c50bfa7dc17d0b7dd3c833a2ee6c4b5d
brosenberg <brosenberg@pistoncloud.com>
2015-02-05 00:32:39 -0800

    Update native names. Fix chooser, add spells

--- dungeonworld/choose.py
+++ dungeonworld/choose.py
@@ -10,7 +10,12 @@ class Chooser(object):
 
     def choose(self):
         for key in self.json.keys():
-            print "%s: %s" % (key, random.choice(self.json[key]))
+            if type(self.json[key]) is list:
+                print "%s: %s" % (key, random.choice(self.json[key]))
+            elif type(self.json[key]) is dict:
+                print "%s: %s" % (key, random.choice(self.json[key].keys()))
+            else:
+                print "Could not generate a %s" % (key,)
 
 def main():
     if len(sys.argv) == 1:
